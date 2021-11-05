9c7c7c6ebd8157493d22b0bb611b370ef93d3b14
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-05 13:22:26 -0300

    Adicionando Card e A

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 
+import Card from "./components/layout/Card";
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio";
 import Fragmento from "./components/basicos/Fragmento";
 import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/Primeiro";
 
-
 export default () => (
   <div id="app">
     <h1>Fundamentos React (Arrow)</h1>
-    <NumeroAleatorio min={1} max={10}/>
-    <Fragmento />
-    <ComParametro
-      titulo="Situação do Aluno"
-      aluno="Fabrício Caldana"
-      nota={8.6}
-    ></ComParametro>
-    <Primeiro />
+
+    <Card titulo="#04 - Desafio Aleatório">
+      <NumeroAleatorio min={1} max={10} />
+    </Card>
+
+    <Card titulo="#03 - Fragmento">
+      <Fragmento />
+    </Card>
+
+    <Card titulo="#02 - Com Parâmetro">
+      <ComParametro
+        titulo="Situação do Aluno"
+        aluno="Fabrício Caldana"
+        nota={8.6}
+      ></ComParametro>
+    </Card>
+
+    <Card titulo="#01 - Primeiro Componente">
+      <Primeiro />
+    </Card>
   </div>
 );
