b752a80cdcf93f9c08513f21b91b71b43210371a
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-05 14:08:43 -0300

    Alterando a cor por objeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 
 import Card from "./components/layout/Card";
@@ -7,29 +8,29 @@ import ComParametro from "./components/basicos/ComParametro";
 import Primeiro from "./components/basicos/Primeiro";
 
 export default () => (
-  <div id="app">
-
+  <div className="App">
     <h1>Fundamentos React (Arrow)</h1>
 
-    <Card titulo="#04 - Desafio Aleatório">
-      <NumeroAleatorio min={1} max={10} />
-    </Card>
+    <div className="Cards">
+      <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
+        <NumeroAleatorio min={1} max={10} />
+      </Card>
 
-    <Card titulo="#03 - Fragmento">
-      <Fragmento />
-    </Card>
+      <Card titulo="#03 - Fragmento" color="#E94C6F">
+        <Fragmento />
+      </Card>
 
-    <Card titulo="#02 - Com Parâmetro">
-      <ComParametro
-        titulo="Situação do Aluno"
-        aluno="Fabrício Caldana"
-        nota={8.6}
-      ></ComParametro>
-    </Card>
+      <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
+        <ComParametro
+          titulo="Situação do Aluno"
+          aluno="Fabrício Caldana"
+          nota={8.6}
+        ></ComParametro>
+      </Card>
 
-    <Card titulo="#01 - Primeiro Componente">
-      <Primeiro />
-    </Card>
-    
+      <Card titulo="#01 - Primeiro Componente" color="#588C73">
+        <Primeiro />
+      </Card>
+    </div>
   </div>
 );
