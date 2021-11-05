4c31dd0aca0d77676d11e2d387c5c697471b667e
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-05 10:48:56 -0300

    Usando Arrow Function

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Primeiro from "./components/basicos/Primeiro";
+import ComParametro from "./components/basicos/ComParametro";
+import Fragmento from "./components/basicos/Fragmento";
+
+export default () => (
+  <div id="app">
+    <h1>Fundamentos React (Arrow)</h1>
+    <Fragmento />
+    <ComParametro
+      titulo="Situação do Aluno"
+      aluno="Fabrício Caldana"
+      nota={8.6}
+    ></ComParametro>
+    <Primeiro />
+  </div>
+);
