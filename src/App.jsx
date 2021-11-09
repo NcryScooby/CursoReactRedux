ddbbf719a05a52865a85a528a9d7c01eb4116347
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-09 08:44:26 -0300

    Criando uma lista de repeticao

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 import Card from "./components/layout/Card";
 
+import ListaAlunos from "./components/repeticao/ListaAlunos";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Familia from "./components/basicos/Familia";
 import NumeroAleatorio from "./components/basicos/NumeroAleatorio";
@@ -15,6 +16,10 @@ export default () => (
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+      <Card titulo="#06 - Repetição" color="#FF4C65">
+        <ListaAlunos />
+      </Card>
+
       <Card titulo="#05 - Componentes com Filhos" color="#00C8F8">
         <Familia sobrenome="Anelli">
           <FamiliaMembro nome="Fabrício" />
---
+++ src/data/alunos.js
@@ -0,0 +1,11 @@
+export default [
+    { id: 1, nome: 'Ana', nota: 6.2 },
+    { id: 2, nome: 'Bia', nota: 9.4 },
+    { id: 3, nome: 'Carlos', nota: 3.7 },
+    { id: 4, nome: 'Daniel', nota: 4.8 },
+    { id: 5, nome: 'Gui', nota: 6.5 },
+    { id: 6, nome: 'Rebeca', nota: 5.8 },
+    { id: 7, nome: 'Arthur', nota: 8.7 },
+    { id: 8, nome: 'Pedro', nota: 8.2 },
+    { id: 9, nome: 'Gustavo', nota: 7.7 },
+]
