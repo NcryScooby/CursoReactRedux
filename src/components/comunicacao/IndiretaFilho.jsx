feec4535876aed92ae7b681e9116d7d4679af799
NcryScooby <fabricio_c_anelli@hotmail.com>
2021-11-09 12:48:47 -0300

    Componenate com Estado

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,13 +1,18 @@
-import react from "react";
+import React from "react";
 
 export default (props) => {
     const callBack = props.quandoClicar
+    const min = 10;
+    const max = 30;
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
+
   return (
     <div>
       <div>Filho</div>
       <button
         onClick=
-        {e => callBack("Fabrício", 21, false)}>
+        {e => callBack("Fabrício", gerarIdade(), gerarNerd())}>
         Fornecer Informações
       </button>
     </div>
--- src/components/comunicacao/IndiretaPai.jsx
+++ src/components/comunicacao/IndiretaPai.jsx
@@ -1,28 +1,29 @@
-import react from "react";
-import IndiretaFilho from './IndiretaFilho'
+import React, { useState } from "react";
+import IndiretaFilho from "./IndiretaFilho";
 
-export default props => {
-    let nome = '';
-    let idade = 0;
-    let nerd = false;
+export default (props) => {
+  const [a, b] = [1, 2];
+  const [nome, setNome] = useState("?");
+  const [idade, setIdade] = useState(0);
+  const [nerd, setNerd] = useState(false);
 
-    // nome idade nerd
-    function fornecerInformacoes(nomeParam, idadeParam, nerdParam){
-        nome = nomeParam;
-        idade = idadeParam;
-        nerd = nerdParam;
+  // nome idade nerd
+  function fornecerInformacoes(nome, idade, nerd) {
+    setNome(nome);
+    setIdade(idade);
+    setNerd(nerd);
+  }
 
-        console.log(nomeParam, idadeParam, nerdParam);
-    }
-
-    return (
-        <div>
-            <div>
-                <span>{nome} </span>
-                <span><strong>{idade}</strong></span>
-                <span> {nerd ? 'Verdadeiro' : 'Falso'}</span>
-            </div>
-            <IndiretaFilho quandoClicar={fornecerInformacoes}/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div>
+        <span>{nome} </span>
+        <span>
+          <strong>{idade}</strong>
+        </span>
+        <span> {nerd ? "Verdadeiro" : "Falso"}</span>
+      </div>
+      <IndiretaFilho quandoClicar={fornecerInformacoes} />
+    </div>
+  );
+};
